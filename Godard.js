122a906af3725dc44a2a229e0bd4a40a6e56a169
Switchbreak <switch@switchbreak.net>
2013-10-02 18:01:26 -0500

    Update Godard.js
    Randomized the link order
    Added test of custom cursor

--- Godard.js
+++ Godard.js
@@ -1,16 +1,21 @@
-document.documentElement.style.webkitFilter='grayscale(100%)';
+document.documentElement.style.webkitFilter = 'grayscale(100%)';
+document.documentElement.style.cursor = "url('http://i.imgur.com/SfxJ4EV.png'), auto";
 
 var links = document.getElementsByTagName("a");
+var linkArray = [];
+for( var i = 0; i < links.length; i++ )
+{
+    linkArray.push( links[i] );
+}
+
 var count = 20;
-for (var i=0; i < links.length; i++) {
-	if( links[i].href.indexOf("http") == 0 )
-	{
-		chrome.runtime.sendMessage({url: links[i].href});
-		
-		if( count-- <= 0 )
-		{
-			break;
-		}		
-	}
+while (linkArray.length > 0 && count > 0) {
+    var i = Math.floor( Math.random() * linkArray.length );
+    if (linkArray[i].href.indexOf("http") == 0) {
+        chrome.runtime.sendMessage({ url: linkArray[i].href });
+        count--;
+    }
+
+    linkArray.splice(i, 1);
 }
 
