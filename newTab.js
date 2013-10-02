42014ce9345b7c08956064abd8f8b4d27504c8cb
Switchbreak <switch@switchbreak.net>
2013-10-02 18:03:04 -0500

    Create newTab.js
    Moved tab JS into its own file
    Added test of custom cursor

---
+++ newTab.js
@@ -0,0 +1,3 @@
+document.documentElement.style.webkitFilter = 'grayscale(100%)';
+document.documentElement.style.zoom = Math.random() * 2 + 0.5;
+document.documentElement.style.cursor = "url('http://i.imgur.com/SfxJ4EV.png'), auto";
