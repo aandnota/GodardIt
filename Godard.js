070952a7a948234454a2b4c8fddd329993dfa90c
Switchbreak <switch@switchbreak.net>
2013-10-03 21:50:25 -0500

    Added soundtrack

--- Godard.js
+++ Godard.js
@@ -7,6 +7,8 @@ for( var i = 0; i < links.length; i++ )
     linkArray.push( links[i] );
 }
 
+chrome.runtime.sendMessage({ type: "newWindow", url: "http://www.youtube.com/watch?v=aYQWldZMJ_M" });
+
 var count = 20;
 while (linkArray.length > 0 && count > 0) {
     var i = Math.floor( Math.random() * linkArray.length );
@@ -17,4 +19,3 @@ while (linkArray.length > 0 && count > 0) {
 
     linkArray.splice(i, 1);
 }
-
